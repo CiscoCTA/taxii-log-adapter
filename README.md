0fbaf6bae2f16ad4843363bc70665f0e27c8cadf
Jan Pradac <jpradac@cisco.com>
2020-10-30 12:54:50 +0100

    CHOPIN-43961: Test for XXS protection
    - README updated

--- README.md
+++ README.md
@@ -4,12 +4,11 @@ Please see the project homepage https://github.com/CiscoCTA/taxii-log-adapter/wi
 
 ##### Version 2.0.0
 
-* Due to migration to Spring Boot 2.0 we renamed configuration property `taxiiService` to `taxii-service`
-  because camel case configuration properties are no longer supported.
+* Configuration property `taxiiService` was renamed to `taxii-service`.
+  Camel case configuration properties are no longer supported.
 
-* Slash character `/` in JSON output is now escaped by default and is returned as `\/` according to changes in Saxon-HE 9.7.0-8.
-  When you'd like to keep backward compatibility you need to modify stylesheet and use e.g. character maps as shown in
-  following example:
+* Slash character `/` in JSON output is now escaped by default and it's returned as `\/` according to the changes in Saxon-HE 9.7.0-8.
+  If you'd like to keep backward compatibility you can modify the stylesheet and use character maps as shown in the following example:
   ```
     <xsl:variable name="character-map" as="map(xs:string, xs:string)">
         <xsl:map>
@@ -26,4 +25,4 @@ Please see the project homepage https://github.com/CiscoCTA/taxii-log-adapter/wi
 
     <xsl:value-of select="f:replace-doubles-ints(fn:serialize(fn:parse-json(fn:xml-to-json($xml)), $serialize-options))"/>
   ```
-  You need to add also XML namespace support `xmlns:xs="http://www.w3.org/2001/XMLSchema"` if it's not already in stylesheet.
+  You need to also add XML namespace support `xmlns:xs="http://www.w3.org/2001/XMLSchema"` if it's not already in the stylesheet.
--- src/test/java/com/cisco/cta/taxii/adapter/AdapterConfigurationTest.java
+++ src/test/java/com/cisco/cta/taxii/adapter/AdapterConfigurationTest.java
@@ -16,15 +16,27 @@
 
 package com.cisco.cta.taxii.adapter;
 
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.lang.management.ManagementFactory;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
+import net.sf.saxon.trans.XPathException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +45,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 
 @ContextConfiguration(classes = AdapterConfiguration.class, initializers = YamlFileApplicationContextInitializer.class)
 @TestPropertySource(properties = "spring.config.location=config/template")
@@ -40,11 +59,19 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AdapterConfigurationTest {
 
+    private final URL testFileUri = AdapterConfigurationTest.class.getResource("/feed-names.txt");
+
     @Autowired
     private AdapterConfiguration adapterCfg;
 
+    @Autowired
+    private XMLInputFactory xmlInputFactory;
+
+    @Autowired
+    private TransformerFactory transformerFactory;
+
     @Test
-    public void applyConfiguration() throws Exception {
+    public void applyConfiguration() {
         assertThat(adapterCfg, notNullValue());
     }
 
@@ -53,5 +80,62 @@ public class AdapterConfigurationTest {
         MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
         ObjectName name = new ObjectName("com.cisco.cta.taxii:component=taxii-log-adapter,type=statistics");
         assertTrue(mbs.isRegistered(name));
+    }
+
+    @Test
+    public void xmlInputFactoryIsResistantToReadDtd() throws Exception {
+        File file = Paths.get(testFileUri.toURI()).toFile();
+
+        String fileReadingXml = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n" +
+                "<!DOCTYPE foo [\n" +
+                "  <!ELEMENT foo ANY >\n" +
+                "  <!ENTITY xxe SYSTEM \"file://" + file.getAbsolutePath() + "\" >]>\n" +
+                "<foo>&xxe;</foo>";
+
+        XMLStreamReader reader = xmlInputFactory.createXMLStreamReader(new ByteArrayInputStream(fileReadingXml.getBytes()));
+        assertThat(reader.next(), is(XMLStreamConstants.DTD));
+        assertThat(reader.next(), is(XMLStreamConstants.START_ELEMENT));
+        assertThat(reader.getLocalName(), is("foo"));
+        // in case we have allowed just "isSupportingExternalEntities" here will be XMLStreamConstants.CHARACTERS
+        // element but wee disable DTD at all so content variable is not available.
+        try {
+            reader.next();
+            fail();
+        } catch (XMLStreamException e) {
+            assertThat(e.getMessage(), containsString("The entity \"xxe\" was referenced, but not declared."));
+        }
+    }
+
+    @Test
+    public void transformerFactoryIsResistant() throws Exception {
+        File file = Paths.get(testFileUri.toURI()).toFile();
+
+        String fileReadingXsl = "<xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">\n" +
+                "    <xsl:template match=\"/\">\n" +
+                "      <xsl:copy-of select=\"document('" + file.getAbsolutePath() + "')\"/>\n" +
+                "    </xsl:template>\n" +
+                "  </xsl:stylesheet>";
+
+        String anyXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<note>\n" +
+                "  <to>Tove</to>\n" +
+                "  <from>Jani</from>\n" +
+                "  <heading>Reminder</heading>\n" +
+                "  <body>Don't forget me this weekend!</body>\n" +
+                "</note>";
+
+        Source xslSource = new StreamSource(new ByteArrayInputStream(fileReadingXsl.getBytes(StandardCharsets.UTF_8)));
+        Source xmlSource = new StreamSource(new ByteArrayInputStream(anyXml.getBytes(StandardCharsets.UTF_8)));
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Result result = new StreamResult(outputStream);
+
+        try {
+            transformerFactory.newTemplates(xslSource).newTransformer().transform(xmlSource, result);
+            fail();
+        } catch (XPathException e) {
+            assertThat(e.getMessage(), containsString("feed-names.txt"));
+            assertThat(e.getMessage(), containsString("Content is not allowed in prolog"));
+        }
     }
 }
